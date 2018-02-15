3c7e84591693fe7420507ae49f0bf784d67e6d4f
Karthi <karthigk44@gmail.com>
2018-02-15 12:33:16 +0530

    Update SnapIn JSFile.js

--- SnapIn JSFile.js
+++ SnapIn JSFile.js
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
-<Title>Snap In Chat</title>
-<head>
-<h1>Chat plugin</h1>
-<link rel="stylesheet" type="text/css" href="https://github.com/karthigk44/Tools-Guide/blob/master/SnapInCSS.css" media="screen" />
 <script type='text/javascript' src='https://service.force.com/embeddedservice/3.1/esw.min.js'></script>
+<link rel="stylesheet" type="text/css" href="https://github.com/karthigk44/Tools-Guide/blob/master/SnapInCSS.css" media="screen" />
 <script type='text/javascript' src='https://github.com/karthigk44/Tools-Guide/blob/master/SnapIn%20JSFile.js'> </script>
 </head>
 </html>
